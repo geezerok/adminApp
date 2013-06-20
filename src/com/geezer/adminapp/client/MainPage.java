554c11d86962bb31c356b8012b7390dd872fddd5
geezerok <geezer84@gmail.com>
2013-06-20 15:44:53 +0300

    Added new files.

---
+++ src/com/geezer/adminapp/client/MainPage.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.geezer.adminapp.client;
+
+import com.google.gwt.user.client.ui.SplitLayoutPanel;
+
+/**
+ * @author geezer
+ *
+ */
+public class MainPage extends SplitLayoutPanel {
+
+	public MainPage() {
+		super();
+		
+	}
+	
+
+}
