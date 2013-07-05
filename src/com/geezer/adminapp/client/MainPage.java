1b8d360407f375411d564cec292fa6e1946ca3b6
geezerok <geezer84@gmail.com>
2013-07-05 12:48:56 +0300

    Finished composed UserForm without action listeners. Next step - hardware form.

--- src/com/geezer/adminapp/client/MainPage.java
+++ src/com/geezer/adminapp/client/MainPage.java
@@ -1,5 +1,5 @@
 /**
- * 
+ * By Oleg Antonov. 2013
  */
 package com.geezer.adminapp.client;
 
@@ -8,6 +8,7 @@ import com.extjs.gxt.ui.client.event.ButtonEvent;
 import com.extjs.gxt.ui.client.event.SelectionListener;
 import com.extjs.gxt.ui.client.util.Margins;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
+import com.extjs.gxt.ui.client.widget.Label;
 import com.extjs.gxt.ui.client.widget.TabItem;
 import com.extjs.gxt.ui.client.widget.TabPanel;
 import com.extjs.gxt.ui.client.widget.button.Button;
@@ -21,98 +22,81 @@ import com.extjs.gxt.ui.client.widget.menu.MenuBar;
 import com.extjs.gxt.ui.client.widget.menu.MenuBarItem;
 import com.extjs.gxt.ui.client.widget.menu.MenuItem;
 import com.google.gwt.core.client.GWT;
-
-import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
  * @author geezer
  * 
  */
 public class MainPage extends ContentPanel {
+
+	private static final int MIN_TAB_WIDTH = 100;
+	private static final int FOOTER_HEIGHT = 20;
+	private static final int LEFT_PANEL_WIDTH = 130;
+	private static final int MENU_BAR_HEIGHT = 25;
+	private static final int MAIN_PAGE_HEIGHT = 600;
+	private static final int MAIN_PAGE_WIDTH = 900;
+	private static final String DEFAULT_STATUS = "by Oleg Antonov 2013";
 	
 	/**
-	 * 
-	 */
-	private static final int FOOTER_HEIGHT = 15;
-
-	/**
-	 * 
-	 */
-	private static final int LEFT_PANEL_WIDTH = 130;
-
-	/**
-	 * 
-	 */
-	private static final int MENU_BAR_HEIGHT = 35;
-
-	/**
-	 * 
-	 */
-	private static final int MAIN_PAGE_HEIGHT = 500;
-
-	/**
-	 * 
-	 */
-	private static final int MAIN_PAGE_WIDTH = 900;
-
-	/**
-	 * The message displayed to the user when the server cannot be 
-	 * reached or returns an error.
+	 * The message displayed to the user when the server cannot be reached or
+	 * returns an error.
 	 */
 	private static final String SERVER_ERROR = "An error occurred while "
 			+ "attempting to contact the server. Please check your network "
 			+ "connection and try again.";
-
 	/**
 	 * The remote service proxy that talks to the server-side service.
 	 */
 	private adminAppServiceAsync adminAppSvc = GWT
 			.create(adminAppService.class);
+
 	private TabPanel mainPanel;
+	private static Label statusFooterLabel = new Label(DEFAULT_STATUS);
 
 	public MainPage() {
 		super(new BorderLayout());
 		setSize(MAIN_PAGE_WIDTH, MAIN_PAGE_HEIGHT);
 		setHeaderVisible(false);
 		mainPanel = new TabPanel();
-		mainPanel.setMinTabWidth(100);
+		mainPanel.setMinTabWidth(MIN_TAB_WIDTH);
 		mainPanel.setTabScroll(true);
 		mainPanel.setCloseContextMenu(true);
-		
-//		Define BorderLayoutData for main components of MainPage. LD states for Layout Data.
-		BorderLayoutData menuBarLD = new BorderLayoutData(
-				LayoutRegion.NORTH, MENU_BAR_HEIGHT);
+
+		// Define BorderLayoutData for main components of MainPage. LD states
+		// for Layout Data.
+		BorderLayoutData menuBarLD = new BorderLayoutData(LayoutRegion.NORTH,
+				MENU_BAR_HEIGHT);
 		menuBarLD.setMargins(new Margins(5));
 
-		BorderLayoutData leftPanelLD = new BorderLayoutData(
-				LayoutRegion.WEST, LEFT_PANEL_WIDTH);
+		BorderLayoutData leftPanelLD = new BorderLayoutData(LayoutRegion.WEST,
+				LEFT_PANEL_WIDTH);
 		leftPanelLD.setSplit(true);
 		leftPanelLD.setCollapsible(true);
 		leftPanelLD.setMargins(new Margins(0, 5, 0, 5));
 
-		BorderLayoutData mainPanelLD = new BorderLayoutData(
-				LayoutRegion.CENTER);
+		BorderLayoutData mainPanelLD = new BorderLayoutData(LayoutRegion.CENTER);
 		mainPanelLD.setMargins(new Margins(0));
 
-		BorderLayoutData footerLD = new BorderLayoutData(
-				LayoutRegion.SOUTH, FOOTER_HEIGHT);
+		BorderLayoutData footerLD = new BorderLayoutData(LayoutRegion.SOUTH,
+				FOOTER_HEIGHT);
 		footerLD.setMargins(new Margins(5));
-		
-//		Fill MainPage with its components.
+
+		// Fill MainPage with its components.
 		setTopComponent(getMainHeader());
 		add(getMenuBar(), menuBarLD);
 		add(mainPanel, mainPanelLD);
 		add(getLeftPanel(), leftPanelLD);
-//		add(getFooter(), footerLD);
-		
-		
-		
+		add(getFooter(), footerLD);
 
 	}
 
-	private Widget getFooter() {
-		// TODO Auto-generated method stub
-		return null;
+	private VerticalPanel getFooter() {
+		VerticalPanel result = new VerticalPanel();
+		result.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_RIGHT);
+		result.add(statusFooterLabel);
+		return result;
 	}
 
 	private ContentPanel getLeftPanel() {
@@ -120,8 +104,7 @@ public class MainPage extends ContentPanel {
 		result.setHeadingText("Side bar");
 		result.setBodyBorder(true);
 		result.setLayout(new AccordionLayout());
-		RowData buttonLayout = new RowData(1, -1,
-				new Margins(5, 5, 5, 5));
+		RowData buttonLayout = new RowData(1, -1, new Margins(5, 5, 5, 5));
 		ContentPanel userPanel = new ContentPanel(new RowLayout());
 		userPanel.setHeadingText("Users");
 		Button addNewUserBtn = new Button("Add new user",
@@ -131,64 +114,64 @@ public class MainPage extends ContentPanel {
 					public void componentSelected(final ButtonEvent ce) {
 						UserForm userForm = new UserForm();
 						addTab("New User", userForm);
-						
+
 					}
 				});
 		Button usersListBtn = new Button("User's List",
 				new SelectionListener<ButtonEvent>() {
-					
+
 					@Override
 					public void componentSelected(ButtonEvent ce) {
 						UsersList usersList = new UsersList();
 						addTab("User's List", usersList);
-						
+
 					}
 				});
-		
+
 		userPanel.add(addNewUserBtn, buttonLayout);
 		userPanel.add(usersListBtn, buttonLayout);
 		result.add(userPanel);
-		
+
 		ContentPanel hardwarePanel = new ContentPanel(new RowLayout());
 		hardwarePanel.setHeadingText("Hardware");
 		Button addNewHardwareBtn = new Button("Add new hardware",
 				new SelectionListener<ButtonEvent>() {
-					
+
 					@Override
 					public void componentSelected(ButtonEvent ce) {
 						HardwareForm hardwareForm = new HardwareForm();
 						addTab("New Hardware", hardwareForm);
-						
+
 					}
 				});
 		Button hardwaresListBtn = new Button("Hardware's List",
 				new SelectionListener<ButtonEvent>() {
-					
+
 					@Override
 					public void componentSelected(ButtonEvent ce) {
 						HardwaresList hardwareList = new HardwaresList();
 						addTab("Hardware's List", hardwareList);
-						
+
 					}
 				});
 		Button addNewCompBtn = new Button("Add new computer",
 				new SelectionListener<ButtonEvent>() {
-					
+
 					@Override
 					public void componentSelected(ButtonEvent ce) {
 						ComputerForm computerForm = new ComputerForm();
 						addTab("New Computer", computerForm);
-						
+
 					}
 				});
 		Button computerssListBtn = new Button("Computer's List",
 				new SelectionListener<ButtonEvent>() {
-					
+
 					@Override
 					public void componentSelected(ButtonEvent ce) {
 						ComputersList computerList = new ComputersList();
 						addTab("Computer's List", computerList);
-						
+
 					}
 				});
 		hardwarePanel.add(addNewCompBtn, buttonLayout);
@@ -196,12 +179,10 @@ public class MainPage extends ContentPanel {
 		hardwarePanel.add(addNewHardwareBtn, buttonLayout);
 		hardwarePanel.add(hardwaresListBtn, buttonLayout);
 		result.add(hardwarePanel);
-		
-		
-		
+
 		return result;
 	}
-	
+
 	private void addTab(String text, ContentPanel tab) {
 		TabItem tabItem = new TabItem();
 		tabItem.setText(text);
@@ -211,9 +192,8 @@ public class MainPage extends ContentPanel {
 		mainPanel.setSelection(tabItem);
 	}
 
-	
 	private MenuBar getMenuBar() {
-		MenuBar result  = new MenuBar();
+		MenuBar result = new MenuBar();
 		Menu fileMenu = new Menu();
 		Menu helpMenu = new Menu();
 		Menu reportMenu = new Menu();
@@ -230,10 +210,10 @@ public class MainPage extends ContentPanel {
 		return result;
 	}
 
-	public ContentPanel getMainHeader() {
+	private ContentPanel getMainHeader() {
 		ContentPanel result = new ContentPanel();
 		result.setHeaderVisible(false);
-		result.add(new com.extjs.gxt.ui.client.widget.Label("geezerok"));
+		result.add(new com.extjs.gxt.ui.client.widget.Label("Admin Application by Antonov Oleg"));
 		return result;
 	}
 
--- src/com/geezer/adminapp/client/UserForm.java
+++ src/com/geezer/adminapp/client/UserForm.java
@@ -3,12 +3,138 @@
  */
 package com.geezer.adminapp.client;
 
+import java.util.Date;
+
+import com.extjs.gxt.ui.client.Style.HorizontalAlignment;
+import com.extjs.gxt.ui.client.widget.button.Button;
+import com.extjs.gxt.ui.client.widget.form.DateField;
 import com.extjs.gxt.ui.client.widget.form.FormPanel;
+import com.extjs.gxt.ui.client.widget.form.Radio;
+import com.extjs.gxt.ui.client.widget.form.RadioGroup;
+import com.extjs.gxt.ui.client.widget.form.SimpleComboBox;
+import com.extjs.gxt.ui.client.widget.form.TextArea;
+import com.extjs.gxt.ui.client.widget.form.TextField;
+import com.extjs.gxt.ui.client.widget.form.TimeField;
+import com.extjs.gxt.ui.client.widget.layout.FormData;
 
 /**
  * @author geezer
  *
  */
 public class UserForm extends FormPanel {
-
+	
+	FormData formData;
+	private TextField<String> aliasName;
+	private TextField<String> firstName;
+	private TextField<String> lastName;
+	private TextField<String> accessKey;
+	private TextField<String> eMail;
+	private TextField<String> localPhone;
+	private TextField<String> mobile;
+	private DateField dateOfBirth;
+	private DateField joinDate;
+	private TextArea description;
+	private Radio maleRadio;
+	private Radio femaleRadio;
+	private SimpleComboBox userGroup;
+	private Button findButton;
+	private Button saveButton;
+	private Button updateButton;
+	private Button deleteButton;
+	private Button clearButton;
+	
+	
+	public UserForm() {
+		createForm();
+	}
+	
+	/**
+	 * 
+	 */
+	private void createForm() {
+		formData = new FormData("100%");
+		setSize(450, -1);
+		setHeadingHtml("User");  
+	    setFrame(true);
+	    setLabelAlign(LabelAlign.LEFT);
+	    setButtonAlign(HorizontalAlignment.CENTER);
+	    
+	    aliasName = new TextField<String>();
+	    aliasName.setFieldLabel("Alias Name");
+	    aliasName.setAllowBlank(false);
+	    aliasName.setEmptyText("Enter the alias name here, required");
+	    aliasName.getFocusSupport().setPreviousId(getButtonBar().getId());
+	    add(aliasName, formData);
+	    	  
+	    firstName = new TextField<String>();  
+	    firstName.setFieldLabel("First name");
+	    firstName.setEmptyText("Enter user's first name");
+	    add(firstName, formData);
+	    
+	    lastName = new TextField<String>();
+	    lastName.setFieldLabel("Last name");
+	    lastName.setEmptyText("Enter user's last name");
+	    add(lastName, formData);
+	    
+	    accessKey = new TextField<String>();
+	    accessKey.setFieldLabel("Access key");
+	    add(accessKey, formData);
+	    
+	    eMail = new TextField<String>();
+	    eMail.setFieldLabel("Email");
+	    add(eMail, formData);
+	    
+	    dateOfBirth = new DateField();  
+	    dateOfBirth.setFieldLabel("Birthday");  
+	    dateOfBirth.setMaxValue(new Date());
+	    add(dateOfBirth, formData);  
+	    
+	    joinDate = new DateField();
+	    joinDate.setFieldLabel("Join date");
+	    joinDate.setValue(new Date());
+	    add(joinDate, formData);
+	    
+	    maleRadio = new Radio();
+	    femaleRadio = new Radio();
+	    maleRadio.setBoxLabel("Male");
+	    femaleRadio.setBoxLabel("Female");
+	    RadioGroup genderRadioGroup = new RadioGroup();
+	    genderRadioGroup.setFieldLabel("Select gender");
+	    genderRadioGroup.add(maleRadio);
+	    genderRadioGroup.add(femaleRadio);
+	    add(genderRadioGroup, formData);
+	    
+	    userGroup = new SimpleComboBox<String>();
+	    userGroup.setFieldLabel("User group");
+	    userGroup.add("Blank");
+	    add(userGroup, formData);
+	    
+	    localPhone = new TextField<String>();
+	    localPhone.setFieldLabel("Phone");
+	    add(localPhone, formData);
+	    
+	    mobile = new TextField<String>();
+	    mobile.setFieldLabel("Mobile");
+	    add(mobile, formData);
+	    
+	    description = new TextArea();
+	    description.setFieldLabel("Commentary");
+	    description.setEmptyText("Enter the commentary here. " +
+	    		"Maximum 140 symbols(Yeah, hello twitter)"); 
+	    description.setPreventScrollbars(true);
+	    description.setMaxLength(140);
+	    add(description, formData);
+	    
+	    findButton = new Button("Find");
+	    saveButton = new Button("Save");
+	    updateButton = new Button("Update");
+	    deleteButton = new Button("Delete");
+	    clearButton = new Button("Clear");
+	    
+	    addButton(findButton);
+	    addButton(saveButton);
+	    addButton(updateButton);
+	    addButton(deleteButton);
+	    addButton(clearButton);
+	}
 }
